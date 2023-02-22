65a0a5c38b0951056b8a2afe4dfa5652a66abc50
Taha Baran <tahabarann@hotmail.com>
2023-02-22 17:53:38 +0300

    Update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -2,33 +2,33 @@
 using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 
-ProductTest();
-//CategoryTest();
+//ProductTest();
+CategoryTest();
 
-static void ProductTest()
-{
-    ProductManager productManager = new ProductManager(new EfProductDal());
+//static void ProductTest()
+//{
+//    ProductManager productManager = new ProductManager(new EfProductDal(), new CategoryManager(EfCategoryDal));
 
-    var result = productManager.GetProductDetails();
-    
-    if (result.Success)
-    {
-        foreach (var product in result.Data)
-        {
-            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
-        }
-    }
-    else 
-    { 
-        Console.WriteLine(result.Message);
-    }
+//    var result = productManager.GetProductDetails();
 
-}
+//    if (result.Success)
+//    {
+//        foreach (var product in result.Data)
+//        {
+//            Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+//        }
+//    }
+//    else 
+//    { 
+//        Console.WriteLine(result.Message);
+//    }
+
+//}
 
 static void CategoryTest()
 {
     CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
-    foreach (var category in categoryManager.GetAll())
+    foreach (var category in categoryManager.GetAll().Data)
     {
         Console.WriteLine(category.CategoryName);
     }
